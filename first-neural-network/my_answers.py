872deea7756659c6282028a5cbb97455eb496850
rampizen <34708246+rampizen@users.noreply.github.com>
2017-12-24 22:53:55 +0530

    Update my_answers.py

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -20,15 +20,15 @@ class NeuralNetwork(object):
         #
         # Note: in Python, you can define a function with a lambda expression,
         # as shown below.
-        self.activation_function = lambda x : 0  # Replace 0 with your sigmoid calculation.
+        #self.activation_function = lambda x : 0  # Replace 0 with your sigmoid calculation.
         
         ### If the lambda code above is not something you're familiar with,
         # You can uncomment out the following three lines and put your 
         # implementation there instead.
         #
-        #def sigmoid(x):
-        #    return 0  # Replace 0 with your sigmoid calculation here
-        #self.activation_function = sigmoid
+        def sigmoid(x):
+           return 1/(1+np.exp(-x))   # Replace 0 with your sigmoid calculation here
+        self.activation_function = sigmoid
                     
 
     def train(self, features, targets):
